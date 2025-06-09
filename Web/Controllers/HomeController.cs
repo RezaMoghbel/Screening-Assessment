dbf7e7e9159d0e2bc71fd9418c03f510363f7ea2
Reza Moghbel <r.moghbel@hotmail.com>
2025-06-09 16:27:15 +0100

    RM Assessment Changes 2025-Jun-09 16:27

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -7,6 +7,12 @@ namespace Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static int _count = 1; // Initialised to 1 due to start value being 1
+        private static bool _shouldResetCount = true; //Should we reset the value? initialised to true 
+        private static string _charReversedOutput = ""; // Output for character reversal
+        private static string _wordReversedOutput = ""; // Output for word reversal
+        private static bool _shouldResetUserInput = true; // Should we reset the user input? initialised to true
+        private static string _stringReversalError = "";// Error message for string reversal
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,9 +21,57 @@ namespace Web.Controllers
 
         public IActionResult Index()
         {
+            if (_shouldResetCount)
+                _count = 1; //Reset the count on page load or refresh
+            _shouldResetCount = true;
+            if (_shouldResetUserInput)
+            {
+                _charReversedOutput = "";
+                _wordReversedOutput = "";
+            }
+            _shouldResetUserInput = true;
+            ViewBag.CharReversed = _charReversedOutput;
+            ViewBag.WordReversed = _wordReversedOutput;
+            ViewBag.Count = _count;
+            ViewBag.StringReversalError = _stringReversalError; // Pass the error message to the view
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Increment()
+        {
+            _shouldResetCount = false; // Don't reset on immediate redirect
+            _count++;
+            return RedirectToAction("Index");
+        }
+        public IActionResult ResetCount()
+        {
+            _shouldResetCount = true;
+            return RedirectToAction("Index");
+        }
+        public IActionResult ClickSummary()
+        {
+            ViewBag.Count = _count;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                _stringReversalError = "String reversal is a required field!"; // Set error message if input is null or empty
+                return RedirectToAction("Index");
+            }
+            _shouldResetUserInput = false;// Don't reset on immediate redirect
+            _stringReversalError = ""; // Clear any previous errors
+            _charReversedOutput = new string(input?.Reverse().ToArray()); // Reverse the characters in the string
+            _wordReversedOutput = input == null ? "" : string.Join(" ", input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse()); // Reverse the words in the string
+            return RedirectToAction("Index"); // Redirect to Index to show the results
+        }
+        public IActionResult ResetReverse()
+        {
+            _shouldResetUserInput = true;
+            return RedirectToAction("Index");
+        }
         public IActionResult Privacy()
         {
             return View();
